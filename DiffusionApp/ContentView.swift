62f1a37fc64eec780f8ed26efed7045206c02ef2
Kyle Jhong <kyle.jhong@itstar.tw>
2025-04-03 12:02:29 +0800

    Initial Commit

---
+++ DiffusionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiffusionApp
+//
+//  Created by jcyuwu on 2025/4/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiffusionApp/DiffusionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiffusionAppApp.swift
+//  DiffusionApp
+//
+//  Created by jcyuwu on 2025/4/3.
+//
+
+import SwiftUI
+
+@main
+struct DiffusionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
