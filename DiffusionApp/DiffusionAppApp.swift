e689104510befd94e2a962657fcfdb4bb32c3f11
Kyle Jhong <kyle.jhong@itstar.tw>
2025-04-03 13:59:33 +0800

    add ios files

--- DiffusionApp/DiffusionAppApp.swift
+++ DiffusionApp/DiffusionAppApp.swift
@@ -8,10 +8,22 @@
 import SwiftUI
 
 @main
-struct DiffusionAppApp: App {
+struct DiffusionApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoadingView()
         }
     }
 }
+
+let runningOnMac = ProcessInfo.processInfo.isMacCatalystApp
+let deviceHas6GBOrMore = ProcessInfo.processInfo.physicalMemory > 5910000000   // Reported by iOS 17 beta (21A5319a) on iPhone 13 Pro: 5917753344
+let deviceHas8GBOrMore = ProcessInfo.processInfo.physicalMemory > 7900000000   // Reported by iOS 17.0.2 on iPhone 15 Pro Max: 8021032960
+
+let deviceSupportsQuantization = {
+    if #available(iOS 17, *) {
+        true
+    } else {
+        false
+    }
+}()
